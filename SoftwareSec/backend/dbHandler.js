3adedeed4e985209af8f854cc3cb033b7f737936
Kyle Riley <KMRiley4136@gmail.com>
2019-04-17 16:45:45 -0400

    Adding bcrypt

--- SoftwareSec/backend/dbHandler.js
+++ SoftwareSec/backend/dbHandler.js
@@ -12,12 +12,6 @@ const selectAll = async () =>{
     const data = await knex.select('*').from('usersTable');
     return data;
 }
-var user = {
-    LoginName: 'egger',
-    PasswordHash: 'testing',
-    FirstName: 'inserting',
-    LastName: 'user',
-}
 const createUser = async () => {
     await knex('usersTable').insert({...user})
 }
