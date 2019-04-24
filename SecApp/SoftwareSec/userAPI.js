370f0a839c9eb93072344547371ed596d0b01dfb
Kyle Riley <KMRiley4136@gmail.com>
2019-04-23 20:40:23 -0400

    added env and userAPI work

---
+++ SecApp/SoftwareSec/userAPI.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config()
+
+const knex = require('knex')({
+    client: 'mssql',
+    connection: {
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : 'SE41781_KRiley'
+    }
+})
+
+app.post('/createuser', function(req, res) {
+    store.createUser({
+        LoginName: req.body.username,
+        PasswordHash: req.body.PasswordHash,
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Email: req.body.Email
+    })
+    .then(() => res.sendStatus(200));
+});
+app.post('/login', function(req, res){
+    try{
+        res.send(knex.select('*').from('usersTable').where('LoginName', req.LoginName).andWhere('PasswordHash', req.PasswordHash));
+    }catch(e){
+        res.send(e);
+    }
+});
+module.exports = {
+    createUser ({LoginName, PasswordHash, FirstName, LastName, Email}) {
+        console.log("Api call createUser");
+        return Promise.resolve();
+    }
+}
+const selectAll = async () =>{
+    const data = await knex.select('*').from('usersTable');
+    return data;
+}
+const createUser = async () => {
+    await knex('usersTable').insert({...req})
+}
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000')
+});
--- SecApp/sec_frontend/src/Views/Login.js
+++ SecApp/sec_frontend/src/Views/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Button, Container, Col, Card } from "react-bootstrap";
+import { Form, Button, Container, Col, Card, ButtonGroup } from "react-bootstrap";
 
 class Login extends Component {
     render() {
@@ -17,14 +17,18 @@ class Login extends Component {
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
                             </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
+                            <ButtonGroup>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                                <Button variant="light">
+                                    Already Have An Account? Click Here!
+                                </Button>
+                            </ButtonGroup>
                         </Form>
                     </Card>
                 </Col>
             </Container>
-
         )
     }
 }
